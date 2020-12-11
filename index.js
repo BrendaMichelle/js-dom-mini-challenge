488bfebdd4328e6d823ef2c4caa2ba783873e865
Ekenayy <ekenayy@gmail.com>
2020-12-11 15:39:29 -0500

    del 3 done

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ header.style.color = "blue"
 
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
-
+console.log('This is what the traveler object looks like: ', traveler)
+profilediv = document.querySelector('div#profile')
+profilediv.children[1].innerText = traveler.name
+profilediv.children[0].src = traveler.photo
+profilediv.querySelector('em').innerText = traveler.nickname
+profilediv.querySelector('img').alt = traveler.name
 
 /***** Deliverable 4 *****/
 
