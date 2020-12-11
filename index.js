583de51fb42fdc24b5de2d8e0f968e4a565e8379
serkansahin55 <ssahin55@outlook.com>
2020-12-11 16:59:48 -0500

    done

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ const header = document.querySelector("#header")
 console.log("Here's your header:", header)
 
 
+
 /***** Deliverable 2 *****/
 
+header.style.color = "red"
 
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+console.log('This is what the traveler object looks like: ', traveler)
 
+let travelerName = document.querySelector('h2')
+let nickname = document.querySelector('div#profile em')
+let imageUrl = document.querySelector('div#profile img')
+
+travelerName.textContent = traveler.name
+nickname.textContent = traveler.nickname
+imageUrl.src = traveler.photo
+imageUrl.alt = "Alternative Pic"
 
 /***** Deliverable 4 *****/
 
-
+// will redo all of this over weekend 
 
 /***** Deliverable 5 *****/
+
