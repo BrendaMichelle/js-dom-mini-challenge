4194fcc63371c4d5a3017da3ab100728fe0fa18f
nohaderf <freda.hon@gmail.com>
2020-12-11 12:33:15 -0500

    done

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 /***** Deliverable 1 *****/
 const header = document.querySelector("#header")
 console.log("Here's your header:", header)
-
+// added defer to the script
 
 /***** Deliverable 2 *****/
 
-
+header.style.color = "teal"
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+console.log('This is what the traveler object looks like: ', traveler)
 
+const name = document.querySelector("h2")
+name.textContent = traveler.name
+
+const nickname = document.querySelector("em")
+nickname.textContent = traveler.nickname
+
+const img = document.querySelector("img")
+img.src = traveler.photo
 
 /***** Deliverable 4 *****/
 
+const ul = document.querySelector("#animals")
+traveler.animalSightings.forEach(function(animalObj){
+     // console.log("works?")
+    const li = document.createElement("li")
+    li.dataset.id = animalObj.id
+
+    const pTag = document.createElement("p")
+    pTag.textContent = animalObj.description
+
+    const animalImg = document.createElement("img")
+    animalImg.src = animalObj.photo
+    animalImg.alt = animalObj.species
+
+    const aTag = document.createElement("a")
+    aTag.href = animalObj.link
+    aTag.target = "_blank"
+    aTag.textContent = animalObj.link
+
+    li.append(animalImg, pTag, aTag)
+    ul.append(li)
+}) 
 
 
 /***** Deliverable 5 *****/
+
+const removeAnimal = document.querySelector("[data-id='3']")
+removeAnimal.remove()
