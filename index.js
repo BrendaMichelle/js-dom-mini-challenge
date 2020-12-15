96a9e300e8d2cbc55ef2693b725be23ceef7a14e
BMoClay <benmorgancleveland@gmail.com>
2020-12-15 11:33:09 -0500

    first commit

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ console.log("Here's your header:", header)
 
 /***** Deliverable 2 *****/
 
-
+header.style.color = "Blue"
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
-
+console.log('This is what the traveler object looks like: ', traveler)
+let img = document.querySelector(#profile.photo)
+let h2 = document.querySelector(#profile.name)
+let em = document.querySelector(#profile.nickname)
 
 /***** Deliverable 4 *****/
-
-
+let animalSightingList = 
+<li data-id="{animalSighting id}">
+  <p>{animal sighting description}</p>
+  <img src="{animal sighting photo}" alt="{animal sighting species}"/>
+  <a href="{animal sighting link}" target="_blank">Here's a video about the {animal sighting species} species!</a>
+</li>
 
 /***** Deliverable 5 *****/
+//didnt have time to get to these two/three deliverables... but also wasnt sure how to complete them either
