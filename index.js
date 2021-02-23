164327b6f0026d66ca22d96c45de0cdde3a617c1
Kwaku <kwakugyapong219@gmail.com>
2021-02-23 15:00:13 -0500

    Dom Challenge

--- index.js
+++ index.js
@@ -5,14 +5,39 @@ console.log("Here's your header:", header)
 
 /***** Deliverable 2 *****/
 
+header.style.color = "red";
 
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+//  console.log('This is what the traveler object looks like: ', traveler)
+const div = document.createElement("div")
+div.innerHTML =    `<div> 
+<img src="${traveler.photo}"
+alt="${traveler.name}">
+<h2> ${traveler.name}</h2>
+<em>${traveler.nickname}</em> </div>`
+
+const parentDiv = document.querySelector("div.traveler")
+parentDiv.prepend(div)
+
+// /***** Deliverable 4 *****/
+const li = document.createElement("li")
 
 
-/***** Deliverable 4 *****/
+traveler.animalSightings.forEach(function (animal){
+    li.innerHTML += `
+<li data-id="${animal.id}">
+  <p>${animal.description}</p>
+  <img src="${animal.photo}" alt="${animal.species}"/>
+  <a href="${animal.link}" target="_blank">Here's a video about the {animal sighting species} species!</a>
+</li>`
 
+})
 
+const parentUl = document.querySelector("ul#animals")
+parentUl.prepend(li)
 
 /***** Deliverable 5 *****/
+
+const k = document.querySelector('[data-id ="3"]')
+k.remove()
