bec50213aad5db62b1a6a225ee02072b238d965f
“Aaron <github email address>
2021-02-23 15:40:11 -0500

    deliverable three completed

--- index.js
+++ index.js
@@ -12,8 +12,20 @@ header.style.color = 'red'
 // I just googled how to use JavaScript to alter an elements color, and this was the first solution that came up.
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+console.log('This is what the traveler object looks like: ', traveler)
+const travelerName = document.querySelector("h2");
+travelerName.innerText = traveler.name;
 
+const travelerNickname = document.querySelector("em");
+travelerNickname.innerText = traveler.nickname;
+
+const travelerImage = document.querySelector("img");
+travelerImage.src = traveler.photo;
+travelerImage.alt = traveler.name;
+
+// I assigned all name, nickname, and image related tags to a variable then used innerText to populate their respective portions of the webpage.
+// At first, I tried using innerContent, however it displayed the accompanying HTML placeholder text, so I found that using innerText rendered the correct data.
+// For the image, I simply assigned the img tag to a variable, then used the data.js file to update its attributes with the appropriate URI for the desired image
 
 /***** Deliverable 4 *****/
 
