81e00660b8098f8c989b6adcff5d7bfe0a0b97fe
github username <github email address>
2021-02-23 22:26:58 -0500

    progress

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ console.log("Here's your header:", header)
 
 /***** Deliverable 2 *****/
 
-
+header.style.color = "orange"
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+console.log('This is what the traveler object looks like: ', traveler)
+const person = document.querySelector(`h2`)
+person.textContent= traveler.name
+const alias = document.querySelector(`em`)
+alias.textContent= traveler.nickname
+const pic = document.querySelector(`img`)
+pic.alt= traveler.name
+pic.src= traveler.photo
+
+
 
 
 /***** Deliverable 4 *****/
