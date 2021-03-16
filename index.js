d6d22dbc6c938abab227fb4e85b8e5580ed640eb
Amit <amitnaipaul@gmail.com>
2021-03-16 17:35:26 -0400

    wip

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
+const header = document.querySelector('h1#header')
 console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
-
+const headerColor = document.querySelector('h1#header')
+headerColor.style.color = "red"
 
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+console.log('This is what the traveler object looks like: ', traveler)
 
+const profile = document.querySelector('div#profile')
+const image = profile.querySelector('img')
+image.src = traveler.photo
+image.alt = traveler.name
+
+const h2 = profile.querySelector('h2')
+h2.textContent = traveler.name
+
+const em = profile.querySelector('em')
+em.textContent = traveler.nickname
 
 /***** Deliverable 4 *****/
 
+traveler.animalSightings.forEach(function (animalSighting){
+
+const li = document.createElement('li')
+li.dataset.id = animalSighting.id
+
+li.innerHtml = `
+<p>${animalSighting.description}</p>
+<img src="${animalSighting.photo}" alt="${animalSighting.species}"/>
+<a href="${animalSighting.link}" target="_blank">Here's a video about the ${animalSighting.species} species!</a>`
+
+const ul = document.querySelector('ul#animals')
+ul.append(li)
+
+
+})
 
 
 /***** Deliverable 5 *****/
