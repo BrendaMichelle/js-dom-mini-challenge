a7e20c3bd9eb4b6605b1e2ae3fbf507cf9afab22
Christopher Kim <chris.kim1990@gmail.com>
2021-04-06 17:08:27 -0400

    mini2

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+const header = document.querySelector(`#header`)
+console.log("Here's your header:")
 
 
 /***** Deliverable 2 *****/
-
+const newHeader = document.querySelector('#header')
+console.log(newHeader.style.color = 'blue')
 
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+console.log('This is what the traveler object looks like: ', traveler)
+const updatedTraveler = document.querySelector('div#profile img')
+updatedTraveler.src = "pics/raffy.jpg"
+updatedTraveler.alt = "Raffy"
 
-
+const updatedName = document.querySelector('div#profile h2')
+updatedName.textContent = "Chris"
+const updatedName2 = document.querySelector('div#profile em')
+updatedName2.textContent = "Kim"
 /***** Deliverable 4 *****/
 
 
