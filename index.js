4dd4369a70d996b5aeb1717717508db6b15ca189
Carlos E. La Torre <amautacelt@gmail.com>
2021-04-06 17:09:21 -0400

    Done

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
-
+const newHeader = document.querySelector("#header")
+console.log(newHeader.style.color = "blue")
 
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+const updatedTravelerImg = document.querySelector("div#profile img")
+updatedTravelerImg.src = "pics/raffy.jpg"
+updatedTravelerImg.alt = "Raffy's picture"
+
+const updatedTravelerName = document.querySelector("div#profile h2")
+updatedTravelerName.textContent = "Carlos"
+
+const updatedTravelerNickname = document.querySelector("div#profile em")
+updatedTravelerNickname.textContent = "Chuck"
+
+console.log('This is what the traveler object looks like: ', traveler)
 
 
 /***** Deliverable 4 *****/
