0e06503f28649b39cc8233e8075ad0ddd7558c6b
coltenappleby <colten.appleby@gmail.com>
2021-04-06 19:58:23 -0400

    completed 1 and 2 with html and css, Seth and Radiyah helped me finish. tried to do 5 on my own with a struggle

--- README.md
+++ README.md
@@ -47,6 +47,7 @@ Figure out what you need to change to give Javascript access to the `h1#header` 
 
 **YOUR NOTES**
 ```
+added h1#
 
 ```
 ___________
--- index.js
+++ index.js
@@ -5,14 +5,35 @@ console.log("Here's your header:", header)
 
 /***** Deliverable 2 *****/
 
+header.style.color = "blue"
 
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+console.log('This is what the traveler object looks like: ', traveler)
+
+const profileDiv = document.querySelector("#profile")
+profileDiv.innerHTML = `
+    <img src = ${traveler.photo} alt = ${traveler.name} >
+    <h2> ${traveler.name} </h2>
+    <em> ${traveler.nickname} </em>
+`
+
 
 
 /***** Deliverable 4 *****/
 
+const animalsUl = document.querySelector("#animals")
+animalsUl.innerHTML = 
+    traveler.animalSightings.forEach(function(sighting)) {
+        <li data-id = "${sighting.id}">
+        <p>${sighting.description}</p>
+        <img src="${sighting.photo}" alt="${sighting.species}"/>
+        <a href="${sighting.link}" target="_blank">Here's a video about the ${sighting} species!</a>
+        </li>
+    }
 
 
 /***** Deliverable 5 *****/
+
+
+
