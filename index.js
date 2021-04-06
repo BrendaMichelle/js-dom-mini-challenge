d258b9912083b0b30252c07055c53fba8d800547
Henry <hvelas93@gmail.com>
2021-04-06 18:33:48 -0400

    done

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 const header = document.querySelector('h1#header')
 console.log("Here's your header:", header)
  // used elements and element selector tool to find the correct tag that Amazon Trip text sits in
-
+// defer the script first in the index.html file
+// use the inspect element tool to find the tag and class i'm searching for
 /***** Deliverable 2 *****/
 
 header.style.color = 'green'
+//we already obtained the h1#header instance...it has attributes
+//found them by checking the console.dir(header)
+
 /***** Deliverable 3 *****/
 console.log('This is what the traveler object looks like: ', traveler)
 
@@ -18,6 +22,9 @@ travelerNickname.textContent = traveler.nickname
 const photo = document.querySelector('div#profile').children[0]
 photo.src = traveler.photo
 photo.alt = "puppy dog"
+
+// FIND THE INDIVIDUAL INSTANCES, GET DOWN TO THE SPECIFIC CHILD
+// CHANGED THE ATTRIBUTE
 
 /***** Deliverable 4 *****/
 
@@ -44,6 +51,14 @@ array.forEach(function(sighting){
 }
 
 createAllSightings(animalSightingsArray)
+
+// FIND THE UL OBJECT
+// STORE THE SIGHTINGS IN AN ARRAY THAT CAN BE ITERATED OVER
+// CREATE A CALLBACK FUNCTION THAT MAKES AN LI, SETS ITS ATTRIBUTES, AND APPENDS TO 
+// THE UL ELEMENT
+// CREATE A FUNCTION THAT TAKES IN THE CALLBACK AND RUNS IT
+// ON THE sightingsArray
+
 
 /***** Deliverable 5 *****/
 
