45ce37362b071a6713abc987c0c24dbb01e17dee
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-07-21 00:06:31 -0400

    done

--- index.js
+++ index.js
@@ -5,14 +5,31 @@ console.log("Here's your header:", header)
 
 /***** Deliverable 2 *****/
 
-
+header.style.color = "green"
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+console.log('This is what the traveler object looks like: ', traveler)
+
+document.querySelector("div#profile img").src = traveler.photo
+document.querySelector("div#profile img").alt = traveler.name
+document.querySelector("div#profile h2").innerText = traveler.name
+document.querySelector("div#profile em").innerText = traveler.nickname
 
 
 /***** Deliverable 4 *****/
+traveler.animalSightings.forEach(function(animal) {
+    // use the combo method from lecture!!
+    const li = document.createElement('li')
+    li.dataset.id = animal.id
+    li.innerHTML = `  <p>${animal.description}</p>
+    <img src="${animal.photo}" alt="${animal.species}"/>
+    <a href="${animal.link}" target="_blank">Here's a video about the ${animal.species} species!</a>`
+
+    const ol = document.querySelector("ul#animals")
+    ol.append(li)
+})
 
 
 
 /***** Deliverable 5 *****/
+document.querySelector("li[data-id='3']").remove()
